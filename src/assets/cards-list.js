60bd81975d1167fddbbd3f8831d6f17a0c59f613
Juan Carlos Pérez Martínez <126732588+celides@users.noreply.github.com>
2023-06-19 07:34:30 -0600

    subiendo archivos

---
+++ src/assets/cards-list.js
@@ -0,0 +1,199 @@
+export const list2 = [
+  {
+    rating: "4",
+    desc: "Vistas de los hoteles",
+    imgSrc: [
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqg5siFywtSULLeH0eaN3k6cBQy_HSnJpJGQ&usqp=CAU",
+      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/lpibo-ew-1656015868.jpg",
+      "https://www.travelandleisure.com/thmb/BJupPeakYV7RY_vQQnmvrKAl7LU=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/soneva-jani-sunset-view-maldives-SONEVA0421-74b37591d80441ce87831a41da518e49.jpg",
+      "https://i0.wp.com/theluxuryeditor.com/wp-content/uploads/2019/02/178793622.jpg?resize=1024%2C683",
+
+    ],
+    price: "20000",
+    date: "15-20 May",
+    title: "Hotel 0000",
+  },
+  {
+    rating: "4",
+    desc: "Vistas de los hoteles",
+    imgSrc: [
+      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/lpibo-ew-1656015868.jpg",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqg5siFywtSULLeH0eaN3k6cBQy_HSnJpJGQ&usqp=CAU",
+
+    ],
+    price: "Oferta",
+    date: "25-20 May",
+    title: "Hotel 3",
+  },
+  {
+    rating: "4",
+    desc: "Vistas de los hoteles",
+    imgSrc: [
+      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/lpibo-ew-1656015868.jpg",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqg5siFywtSULLeH0eaN3k6cBQy_HSnJpJGQ&usqp=CAU",
+    ],
+    price: "45000",
+    date: "10-20 Oct",
+    title: "Hotel 3",
+  },
+  {
+    rating: "4",
+    desc: "Vistas de los hoteles",
+    imgSrc: [
+
+      "https://www.travelandleisure.com/thmb/BJupPeakYV7RY_vQQnmvrKAl7LU=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/soneva-jani-sunset-view-maldives-SONEVA0421-74b37591d80441ce87831a41da518e49.jpg",
+      "https://i0.wp.com/theluxuryeditor.com/wp-content/uploads/2019/02/178793622.jpg?resize=1024%2C683",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqg5siFywtSULLeH0eaN3k6cBQy_HSnJpJGQ&usqp=CAU",
+
+    ],
+    price: "20000",
+    date: "15-20 May",
+    title: "Hotel 1",
+  },
+];
+
+export const list = [
+  {
+    rating: "4",
+    desc: "Vistas de los hoteles",
+    imgSrc: [
+
+      "https://www.travelandleisure.com/thmb/BJupPeakYV7RY_vQQnmvrKAl7LU=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/soneva-jani-sunset-view-maldives-SONEVA0421-74b37591d80441ce87831a41da518e49.jpg",
+      "https://i0.wp.com/theluxuryeditor.com/wp-content/uploads/2019/02/178793622.jpg?resize=1024%2C683",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqg5siFywtSULLeH0eaN3k6cBQy_HSnJpJGQ&usqp=CAU",
+    ],
+    price: "20000",
+    date: "15-20 May",
+    title: "Hotel 1",
+  },
+  {
+    rating: "4",
+    desc: "Vistas de los hoteles",
+    imgSrc: [
+
+      require("./hotels/hotel-5.jpeg")
+    ],
+    price: "30000",
+    date: "25-20 May",
+    title: "Hotel 3",
+  },
+  {
+    rating: "4",
+    desc: "Vistas de los hoteles",
+    imgSrc: [
+      require("./hotels/hotel-4.jpeg")
+    ],
+    price: "45000",
+    date: "10-20 Oct",
+    title: "Hotel 3",
+  },
+  {
+    rating: "4",
+    desc: "Vistas de los hoteles",
+    imgSrc: [
+      require("./hotels/hotel-3.jpeg")
+    ],
+    price: "20000",
+    date: "15-20 May",
+    title: "Hotel 1",
+  },
+  {
+    rating: "4",
+    desc: "Vistas de los hoteles",
+    imgSrc: [
+      "https://i0.wp.com/theluxuryeditor.com/wp-content/uploads/2019/02/178793622.jpg?resize=1024%2C683",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqg5siFywtSULLeH0eaN3k6cBQy_HSnJpJGQ&usqp=CAU",
+    ],
+    price: "20000",
+    date: "15-20 May",
+    title: "Hotel 1",
+  },
+  {
+    rating: "4",
+    desc: "Vistas de los hoteles",
+    imgSrc: [
+      require("./imanes/hotel-9.jpg")
+    ],
+    price: "20000",
+    date: "15-20 May",
+    title: "Hotel 1",
+  },
+  {
+    rating: "4",
+    desc: "Vistas de los hoteles",
+    imgSrc: [
+      require("./imanes/hotel-10.jpg")
+    ],
+    price: "20000",
+    date: "15-20 May",
+    title: "Hotel 1",
+  },
+  {
+    rating: "5",
+    desc: "Vistas de los hoteles",
+    imgSrc: [
+      require("./imanes/hotel-6.jpg")
+    ],
+    price: "20000",
+    date: "15-20 May",
+    title: "Hotel 1",
+  },
+  {
+    rating: "4",
+    desc: "Vistas de los hoteles",
+    imgSrc: [
+
+      "https://www.travelandleisure.com/thmb/BJupPeakYV7RY_vQQnmvrKAl7LU=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/soneva-jani-sunset-view-maldives-SONEVA0421-74b37591d80441ce87831a41da518e49.jpg",
+
+    ],
+    price: "10000",
+    date: "15-20 May",
+    title: "Hotel 1",
+  },
+  {
+    rating: "4",
+    desc: "Vistas de los hoteles",
+    imgSrc: [
+      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/lpibo-ew-1656015868.jpg",
+      "https://www.travelandleisure.com/thmb/BJupPeakYV7RY_vQQnmvrKAl7LU=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/soneva-jani-sunset-view-maldives-SONEVA0421-74b37591d80441ce87831a41da518e49.jpg",
+      "https://i0.wp.com/theluxuryeditor.com/wp-content/uploads/2019/02/178793622.jpg?resize=1024%2C683",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqg5siFywtSULLeH0eaN3k6cBQy_HSnJpJGQ&usqp=CAU",
+
+    ],
+    price: "20000",
+    date: "15-20 May",
+    title: "Hotel 1",
+  },
+  {
+    rating: "5",
+    desc: "Vistas de los hoteles",
+    imgSrc: [
+      require("./imanes/hotel-7.jpg")
+    ],
+    price: "20000",
+    date: "15-20 May",
+    title: "Hotel 1",
+  },
+  {
+    rating: "4",
+    desc: "Vistas de los hoteles",
+    imgSrc: [
+
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqg5siFywtSULLeH0eaN3k6cBQy_HSnJpJGQ&usqp=CAU",
+    ],
+    price: "20000",
+    date: "15-20 May",
+    title: "Hotel 1",
+  },
+  {
+    rating: "4",
+    desc: "Vistas de los hoteles",
+    imgSrc: [
+
+      require("./imanes/hotel-12.jpg")
+    ],
+    price: "20000",
+    date: "15-20 May",
+    title: "Hotel 1",
+  },
+];
---
+++ src/assets/images-links.js
@@ -0,0 +1,118 @@
+export const links = [
+  {
+    label: "Asombroso",
+    imgSrc:
+      "https://a0.muscache.com/pictures/3b1eb541-46d9-4bef-abc4-c37d77e3c21b.jpg",
+
+  },
+  {
+    label: "Cima del mundo",
+    imgSrc:
+      "https://a0.muscache.com/pictures/248f85bf-e35e-4dc3-a9a1-e1dbff9a3db4.jpg",
+  },
+  {
+    label: "Granjas",
+    imgSrc:
+      "https://a0.muscache.com/pictures/aaa02c2d-9f0d-4c41-878a-68c12ec6c6bd.jpg",
+  },
+  {
+    label: "Surf",
+    imgSrc:
+      "https://a0.muscache.com/pictures/957f8022-dfd7-426c-99fd-77ed792f6d7a.jpg",
+  },
+  {
+    label: "Campismo",
+    imgSrc:
+      "https://a0.muscache.com/pictures/677a041d-7264-4c45-bb72-52bff21eb6e8.jpg",
+  },
+  {
+    label: "Dulce hogar",
+    imgSrc:
+      "https://a0.muscache.com/pictures/eb7ba4c0-ea38-4cbb-9db6-bdcc8baad585.jpg",
+  },
+  {
+    label: "Seguridad",
+    imgSrc:
+      "https://a0.muscache.com/pictures/c0fa9598-4e37-40f3-b734-4bd0e2377add.jpg",
+  },
+  {
+    label: "Albercas",
+    imgSrc:
+      "https://a0.muscache.com/pictures/3fb523a0-b622-4368-8142-b5e03df7549b.jpg",
+  },
+  {
+    label: "Fogatas ",
+    imgSrc:
+      "https://a0.muscache.com/pictures/3726d94b-534a-42b8-bca0-a0304d912260.jpg",
+  },
+  {
+    label: "Playas ",
+    imgSrc:
+      "https://a0.muscache.com/pictures/bcd1adc0-5cee-4d7a-85ec-f6730b0f8d0c.jpg",
+  },
+  {
+    label: "Playas ",
+    imgSrc:
+      "https://a0.muscache.com/pictures/c5a4f6fc-c92c-4ae8-87dd-57f1ff1b89a6.jpg",
+  },
+  {
+    label: "Castillos ",
+    imgSrc:
+      "https://a0.muscache.com/pictures/33dd714a-7b4a-4654-aaf0-f58ea887a688.jpg",
+  },
+  {
+    label: "cabañas ",
+    imgSrc:
+      "https://a0.muscache.com/pictures/33dd714a-7b4a-4654-aaf0-f58ea887a688.jpg",
+  },
+  {
+    label: "islas ",
+    imgSrc:
+      "https://a0.muscache.com/pictures/ee9e2a40-ffac-4db9-9080-b351efc3cfc4.jpg",
+  },
+  {
+    label: "hoteles",
+    imgSrc:
+      "https://a0.muscache.com/pictures/eb7ba4c0-ea38-4cbb-9db6-bdcc8baad585.jpg",
+  },
+  {
+    label: "Moteles ",
+    imgSrc:
+      "https://a0.muscache.com/pictures/c0fa9598-4e37-40f3-b734-4bd0e2377add.jpg",
+  },
+  {
+    label: "acuática ",
+    imgSrc:
+      "https://a0.muscache.com/pictures/3fb523a0-b622-4368-8142-b5e03df7549b.jpg",
+  },
+  {
+    label: "salvamento ",
+    imgSrc:
+      "https://a0.muscache.com/pictures/3726d94b-534a-42b8-bca0-a0304d912260.jpg",
+  },
+  {
+    label: "casas en playa ",
+    imgSrc:
+      "https://a0.muscache.com/pictures/bcd1adc0-5cee-4d7a-85ec-f6730b0f8d0c.jpg",
+  },
+  {
+    label: "lugares exóticos",
+    imgSrc:
+      "https://a0.muscache.com/pictures/c5a4f6fc-c92c-4ae8-87dd-57f1ff1b89a6.jpg",
+  },
+  {
+    label: "graneros",
+    imgSrc:
+      "https://a0.muscache.com/pictures/33dd714a-7b4a-4654-aaf0-f58ea887a688.jpg",
+  },
+  {
+    label: "gotcha",
+    imgSrc:
+      "https://a0.muscache.com/pictures/33dd714a-7b4a-4654-aaf0-f58ea887a688.jpg",
+  },
+  {
+    label: "atardeceres",
+    imgSrc:
+      "https://a0.muscache.com/pictures/ee9e2a40-ffac-4db9-9080-b351efc3cfc4.jpg",
+  },
+];
